c9a4e5201f75e2a773826cda69874f0ed542a08c
olzama <olga.zamaraeva@gmail.com>
2024-12-04 17:11:55 +0100

    look at coverage by length

--- compute_coverage.py
+++ compute_coverage.py
@@ -2,7 +2,15 @@ import sys, os
 from delphin import itsdb
 
 def compute_coverage(dataset_path):
+    cov_by_len = {}
     dataset = itsdb.TestSuite(dataset_path)
+    proc_items = dataset.processed_items()
+    for proc_it in proc_items:
+        if proc_it['i-length'] not in cov_by_len:
+            cov_by_len[proc_it['i-length']] = 0
+        if len(proc_it.results()) > 0:
+            cov_by_len[proc_it['i-length']] += 1
+    total_cov = sum(cov_by_len.values())
     parsed = len(dataset['result'])
     return parsed / len(dataset['item'])
 
