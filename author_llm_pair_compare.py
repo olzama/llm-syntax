38984177231c6dbf5ee19f9c2358d56d01e1d46e
olzama <olga.zamaraeva@gmail.com>
2025-01-22 17:00:29 +0100

    Computed all cosines, between humans, humans and llms, and between llms

--- author_llm_pair_compare.py
+++ author_llm_pair_compare.py
@@ -3,6 +3,8 @@ import numpy as np
 from scipy import stats
 import matplotlib.pyplot as plt
 import seaborn as sns
+from util import compute_cosine
+from construction_frequencies import combine_types
 
 def insert_zero_counts(dict1, dict2):
     all_ctypes = {'constr': set(), 'lexrule': set(), 'lextype': set()}
@@ -24,4 +26,29 @@ if __name__ == '__main__':
     with open('/mnt/kesha/llm-syntax/analysis/frequencies-json/frequencies-models.json', 'r') as f:
         model_frequencies = json.load(f)
     insert_zero_counts(author_frequencies, model_frequencies)
+    # Combine the two frequency dicts, excluding 'original', 'wsj', and 'wikipedia':
+    authors_and_models = {}
+    for ctype in ['constr', 'lexrule', 'lextype']:
+        authors_and_models[ctype] = {}
+        for author in author_frequencies[ctype]:
+            authors_and_models[ctype][author] = author_frequencies[ctype][author]
+        for model in model_frequencies[ctype]:
+            if model not in ['original', 'wsj', 'wikipedia']:
+                authors_and_models[ctype][model] = model_frequencies[ctype][model]
+    # Compute cosine similarities:
+    all_data = combine_types(authors_and_models, ['constr', 'lexrule', 'lextype'])
+    all_cosines = compute_cosine(all_data)
+    human_authors = author_frequencies['constr'].keys()
+    llm_names = [name for name in model_frequencies['constr'].keys() if name not in ['original', 'wsj', 'wikipedia']]
+    # Separate the cosines into human-human, human-llm, and ll:
+    human_human_cosines = {}
+    human_llm_cosines = {}
+    llm_llm_cosines = {}
+    for pair in all_cosines:
+        if pair[0] in human_authors and pair[1] in human_authors:
+            human_human_cosines[pair] = all_cosines[pair]
+        elif pair[0] in human_authors and pair[1] in llm_names:
+            human_llm_cosines[pair] = all_cosines[pair]
+        elif pair[0] in llm_names and pair[1] in llm_names:
+            llm_llm_cosines[pair] = all_cosines[pair]
     print(5)
